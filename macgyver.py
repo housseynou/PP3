b76342b386eabad09b0793a2ac701383864182b4
housseynou coulibaly <housseynou7@gmail.com>
2020-01-31 05:54:29 +0100

    fluidifier les mouvements

--- macgyver.py
+++ macgyver.py
@@ -36,30 +36,40 @@ nb_zone = [
     ]
 
 
+class Surface(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.mur = pygame.image.load(os.path.join('data', 'mur.png')).convert()
+        self.long_mur = 30
+        self.large_mur = 30
+
+    def generer_zone(self):
+        self.fenetre = nb_zone
+        for ligne in nb_zone:
+            self.zone = []
+            for sprite in ligne:
+                self.zone.append(sprite)
+            self.fenetre.append(ligne)
 
 
-def generer_zone():
-    fenetre = []
-    for ligne in nb_zone:
-        zone = []
-        for sprite in ligne:
-            zone.append(sprite)
-        fenetre.append(ligne)
+    def afficher_zone(self):
 
-def afficher_zone():
-    mur = pygame.image.load(os.path.join('data', 'mur.png')).convert()
-    num_ligne = 0
-    for ligne in nb_zone:
-        num_zone = 0
-        for sprite in ligne:
-            x = num_ligne * large_mur
-            y = num_zone * long_mur
+        num_ligne = 0
+        for ligne in nb_zone:
+            num_zone = 0
+            for sprite in ligne:
+                x = num_ligne * self.large_mur
+                y = num_zone * self.long_mur
 
-            if sprite == 'm':
-                ecran.blit(mur, (x, y))
-            elif sprite == 'a':
-                ecran.blit(arrive, (x,y))
-                print(sprite)
+                if sprite == 'm':
+                    ecran.blit(mur, (x, y))
+                elif sprite == 'a':
+                    ecran.blit(arrive, (x,y))
+                num_zone += 1
+            num_ligne += 1
+
+surface = Surface()
 
 
 class Macgyver(pygame.sprite.Sprite):
@@ -67,11 +77,13 @@ class Macgyver(pygame.sprite.Sprite):
         super().__init__()
         self.player = pygame.image.load(os.path.join('data', 'macGyver.png')).convert_alpha()
         self.position = self.player.get_rect()
-        self.mobility = 3
+        self.mobility = 5
+        self.fast = {}
 
     def move_right(self):
-        if event.type == KEYDOWN and event.key == K_RIGHT:
-            self.position.x += self.mobility
+        for event in pygame.event.get():
+            if event.type == KEYDOWN and event.key == K_RIGHT:
+                self.position.x += self.mobility
     def move_left(self):
         if event.type == KEYDOWN and event.key == K_LEFT:
             self.position.x -= self.mobility
@@ -93,16 +105,26 @@ while continuer:
     ecran.blit(background, (0, 0))
     ecran.blit(macgyver.player, macgyver.position)
 
+# fludifier les mouvements du joueur et eviter qu'il sorte du cadre
+
+    if macgyver.fast.get(pygame.K_RIGHT) and macgyver.position.x < 450:
+        macgyver.move_right()
+    elif macgyver.fast.get(pygame.K_LEFT) and macgyver.position.x > 0:
+        macgyver.move_left()
+    elif macgyver.fast.get(pygame.K_UP) and macgyver.position.y > 0:
+        macgyver.move_up()
+    elif macgyver.fast.get(pygame.K_DOWN) and macgyver.position.y < 450:
+        macgyver.move_down()
+
+
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = 0
-        elif event.type == KEYDOWN and event.key == K_RIGHT:
-            macgyver.move_right()
-        elif event.type == KEYDOWN and event.key == K_LEFT:
-            macgyver.move_left()
-        elif event.type == KEYDOWN and event.key == K_UP:
-            macgyver.move_up()
-        elif event.type == KEYDOWN and event.key == K_DOWN:
-            macgyver.move_down()
+        elif event.type == KEYDOWN:
+            macgyver.fast[event.key] = True
+        elif event.type == KEYUP:
+            macgyver.fast[event.key] = False
+        print(macgyver.fast)
+# chargement de la page
 
         pygame.display.flip()
